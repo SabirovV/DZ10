473f4d4061a03a76d97ef72490e3a3d20d0a22f1
SabirovV <66843450+SabirovV@users.noreply.github.com>
2020-07-27 15:12:48 +0300

    Update 10.js

--- 10.js
+++ 10.js
@@ -1,4 +1,4 @@
- function sum (val) {
+    function sum (val) {
         var temp = val;
         sum = function() {
             var arg = arguments[0] ? arguments[0] : 0;
@@ -9,3 +9,28 @@
     
     console.log(sum(3),sum(5),sum(20));
 
+    function makeCounter() {
+        var currentCount = 5;
+      
+        return {
+          getNext: function() {
+            return currentCount++;
+          },
+      
+          set: function(value) {
+            currentCount = value;
+          },
+      
+          reset: function() {
+            currentCount = 5;
+          }
+        };
+      }
+      
+      var counter = makeCounter();
+      
+      alert( counter.getNext() );
+      alert( counter.getNext() );
+      
+      counter.set(20);
+      alert( counter.getNext() );
